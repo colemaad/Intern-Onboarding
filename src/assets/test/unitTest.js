b3abb0d8c95e6e782cd7145bdf1395cb7cb9604c
Abraham <abraham.coleman@banno.com>
2015-07-17 11:47:39 -0500

    Slimming down resources/adding tests

--- src/assets/test/unitTest.js
+++ src/assets/test/unitTest.js
@@ -20,44 +20,56 @@ describe('Main Test ', function() {
 
   });
 
-	var data = {
-		firstName: "Aber",
-		lastName: "Coler",
-		phone: "239-678-9999",
-		email: "coler@banno.com"
-	};
 
 	describe('user Controller', function() {
-		var peeps, scope;
-		beforeEach(inject(function ($rootScope, $controller) {
+		var data = {
+			firstName: "Aber",
+			lastName: "Coler",
+			phone: "239-678-9999",
+			email: "coler@banno.com"
+		};
+		var peeps;
+		var scope;
+		beforeEach(inject(function ($rootScope, $controller, _userService_) {
 			scope = $rootScope.$new();
 			peeps = $controller('usersController', {$scope: scope});
-
+			userService = _userService_;
 		}));
+
+		//Variables
 		describe('Scope Variables', function(){
 			it('should set orderRadio', function() {
 				expect('app.usersController.orderRadio').not.toBe(null);
 			});
-			it('should set orderRadio', function() {
+			it('should set order', function() {
 				expect('app.usersController.$scope.reverse').toBeFalsy;
 			});
-			it('should set orderRadio', function() {
+			it('should have email', function() {
 				expect('app.usersController.$scope.uniqueEmail').toBeTruthy;
 			});
-
-
 		});
-
+		
+		//Functions
 		describe('Scope Functions', function() {
-			it('should set orderRadio', function() {
+
+			it('should set create copy of user to edit', function() {
 				expect('app.usersController.createEditUser').not.toBe(null);
 			});
-			it('should set orderRadio', function() {
-				expect('app.usersController.addUser').not.toBe(null);
+			it('should select user', function() {
+				scope.selectUser(data);
+				expect(scope.user).toBe(data);
+			});
+			it('should delete user', function() {
+				spyOn(userService, 'deleteUser');
+				scope.deleteUser(data);
+				expect(userService.deleteUser).toHaveBeenCalled();
+			});
+			it('should delete a user', function() {
+				spyOn(userService, 'deleteUser');
+				scope.deleteUser(data);
+				expect(userService.deleteUser).not.toThrowError(TypeError);
 			});
 
 		});
-
-
 	});
 });
